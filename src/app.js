a09c77e24d443d52b6784220116e326c238b2599
Kaden Stout <52681889+kadenstout009@users.noreply.github.com>
2020-02-07 18:13:20 -0700

    Second commit to repository, module 1

--- src/app.js
+++ src/app.js
@@ -4,3 +4,12 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+
+app.set(path.join('views',__dirname, 'views'));
+app.set('view enginge', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('index', { title: 'Index'}));
+
+app.listen(3000, () => console.log('PS Project Running on port 3000!'));
