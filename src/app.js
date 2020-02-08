6877488d144c8df26b5f4207ce473dcf289899cf
Kaden Stout <52681889+kadenstout009@users.noreply.github.com>
2020-02-07 18:30:42 -0700

    fix to find view engine

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view enginge', 'ejs');
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
